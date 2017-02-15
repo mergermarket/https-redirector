d371df59b95218e93b0cf1a5b644bb62ccbc8dc9
Tom Yandell <tom@tomyandell.co>
2017-02-15 17:13:58 +0000

    Fix formatting

--- app.go
+++ app.go
@@ -4,14 +4,14 @@ import "net/http"
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	target := "https://" + r.Host + r.URL.Path
-    if len(r.URL.RawQuery) > 0 {
-        target += "?" + r.URL.RawQuery
-    }
+	if len(r.URL.RawQuery) > 0 {
+		target += "?" + r.URL.RawQuery
+	}
 	http.Redirect(w, r, target, http.StatusFound)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":80", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
 }
 
