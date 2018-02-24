d5e80e9b6ec310ce4556122cba1d9e91c956a7f1
Iraklis Alexopoulos <alexopoulosiraklis@gmail.com>
2018-02-24 15:05:50 +0400

    Add a health check route for AWS ELB.
    ELB needs a health check to pass to keep the service running.

--- app.go
+++ app.go
@@ -1,6 +1,13 @@
 package main
 
 import "net/http"
+import "encoding/json"
+import "time"
+
+type HealthCheckStatus struct {
+	Status string
+	Date time.Time
+}
 
 func handler(res http.ResponseWriter, req *http.Request) {
 	target := "https://" + req.Host + req.URL.Path
@@ -10,7 +17,23 @@ func handler(res http.ResponseWriter, req *http.Request) {
 	http.Redirect(res, req, target, http.StatusMovedPermanently)
 }
 
+func healthCheckHandler(res http.ResponseWriter, req *http.Request) {
+	healthCheckStatus := HealthCheckStatus{}
+	healthCheckStatus.Status = "OK"
+	healthCheckStatus.Date = time.Now().Local()
+
+	healthCheckStatusJson, err := json.Marshal(healthCheckStatus)
+	if err != nil{
+		panic(err)
+	}
+
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(http.StatusOK)
+	res.Write(healthCheckStatusJson)
+}
+
 func main() {
+	http.HandleFunc("/health-check", healthCheckHandler)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":80", nil)
 }
